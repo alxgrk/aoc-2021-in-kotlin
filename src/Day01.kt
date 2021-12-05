f996f913749a4eae54633e0c5543faacc070b78e
Alexander Girke <alexander.girke@thing-it.com>
2021-12-06 00:26:13 +0100

    day1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,35 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { it.toInt() }
+        if (numbers.isEmpty()) return 0
+
+        return numbers
+            .fold(0 to numbers[0]) { (count, prev), next ->
+                if (prev < next) count + 1 to next
+                else count to next
+            }.first
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val numbers = input.map { it.toInt() }
+        if (numbers.size < 4) return 0
+
+        val initial = numbers.take(3).sum()
+        return numbers
+            .windowed(3) { it.sum() }
+            .fold(0 to initial) { (count, prev), next ->
+                if (prev < next) count + 1 to next
+                else count to next
+            }.first
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testResult = part1(testInput)
+    check(testResult == 7)
+
+    val testResult2 = part2(testInput)
+    check(testResult2 == 5)
 
     val input = readInput("Day01")
     println(part1(input))
